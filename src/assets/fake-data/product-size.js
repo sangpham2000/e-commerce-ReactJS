a46483d17b0181a5a402f90f302d87f029425df6
sangpham2000 <sangpham15022000@gmail.com>
2022-04-06 16:53:15 +0700

    first commit

---
+++ src/assets/fake-data/product-size.js
@@ -0,0 +1,28 @@
+const size = [
+    {
+        display: "XS",
+        size: "xs"
+    },
+    {
+        display: "S",
+        size: "s"
+    },
+    {
+        display: "M",
+        size: "m"
+    },
+    {
+        display: "L",
+        size: "l"
+    },
+    {
+        display: "XL",
+        size: "xl"
+    },
+    {
+        display: "XXL",
+        size: "xxl"
+    },
+]
+
+export default size
---
+++ src/components/Layout.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Header from './Header'
+import Footer from './Footer'
+import MyRoutes from '../routes/MyRoutes'
+
+import ProductViewModal from './ProductViewModal'
+
+const Layout = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/*" element={
+                    <div>
+                        <Header />
+                        <div className="container">
+                            <div className="main">
+                                <MyRoutes />
+                            </div>
+                        </div>
+                        <Footer />
+                        <ProductViewModal />
+                    </div>
+                }
+                />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Layout
